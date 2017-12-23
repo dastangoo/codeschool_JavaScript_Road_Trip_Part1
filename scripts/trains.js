53695e402b9b581f50a1c78894b9bdb6fb82f1bd
dastangoo <hamed.dastangoo@gmail.com>
2017-12-23 14:40:52 +0330

    Down to the Files

---
+++ scripts/trains.js
@@ -0,0 +1,12 @@
+var totalTrains = 12;
+var trainsOperational = 8;
+// console.log("There are " + trainsOperational + " running trains.");
+console.log("Train #" + 1+ " is running.");
+console.log("Train #" + 2+ " is running.");
+console.log("Train #" + 3+ " is running.");
+console.log("Train #" + 4+ " is running.");
+console.log("Train #" + 5+ " is running.");
+console.log("Train #" + 6+ " is running.");
+console.log("Train #" + 7+ " is running.");
+console.log("Train #" + 8+ " is running.");
+// console.log(trainsOperational == totalTrains);
